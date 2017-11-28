38f6c1d938078c988b3b7d4cb18316d30571744f
fharshitkumar <31175073+fharshitkumar@users.noreply.github.com>
2017-11-28 11:47:35 -0600

    ViewManagementEngine

--- GenXServiceEngagementCenterApplication/src/models/V_ViewManagementEngine.java
+++ GenXServiceEngagementCenterApplication/src/models/V_ViewManagementEngine.java
@@ -9,8 +9,54 @@ import javafx.collections.ObservableList;
 import tableviews.CustomerInfoView;
 import tableviews.PastIncidentView;
 
+
+/**
+ * ********************************************************************************************************
+ * Class		: V_ViewManagementEngine		
+ * Description	: Allows the application components to access the Backend Oracle Views 
+ *                and generate a proper structured View that is consumable for further functionalities.  
+ *	.............................................................................
+ *	....+-------------------------------------------------------------------+....						
+ *	....|            V_ViewManagementEngine Class                   		|....						
+ *	....|               << concrete class >>                        		|....	                    
+ *	....+-------------------------------------------------------------------+....										
+ *	....| [+] PastIncidentView()                                            |....						
+ *	....| [+] ObservableList<CustomerInfoView>: customerinfoviewer(int)     |....						
+ *	....| [+] ObservableList<PastIncidentView>: pastincidentviewer(int)     |....							
+ *	....+-----------------------------------------------------------+....						
+ *	.....................................................................																				
+ * ********************************************************************************************************																								
+ * @author HARSHIT KUMAR 			
+ * Date			    : 28 November, 2017
+ * Source File name	: V_ViewManagementEngine.java       	
+ * 
+ *
+*/
 public class V_ViewManagementEngine {
 
+	/**
+	 * This Function is used to get all the details of the customers which includes following information.
+	 * 		Customer ID
+	 *		Customer Contract ID
+	 *		Customer First Name;
+	 *		Customer Last Name;
+	 *		Customer Contact;
+	 *		Customer residential Address;
+	 *		Customer Gender;
+	 *		Customer Date of birth;
+	 *		Customer residential city;
+	 *		Customer residential state;
+	 *		Customer residential zipcode;
+	 *		Customer role id;
+	 *		Customer role name;
+	 *		Customer role desc;
+	 *		Customer service id(s) ;
+	 *		Customer service name(s);
+	 *		Customer email;
+	 * @param customer
+	 * @return ObservableList<CustomerInfoView>
+	 */
+	
 	public ObservableList<CustomerInfoView> customerinfoviewer(int customer){
 
 		A_DatabaseCommunicationEngine DCE = new A_DatabaseCommunicationEngine();
@@ -43,7 +89,9 @@ public class V_ViewManagementEngine {
 			String servicename;
 			String email;
 
-
+			/***************************************************************************************/
+			/************-----| GET ALL THE RECORD ROWS DATA INTO A STRUCTURED FORMAT |------- *****/
+			/***************************************************************************************/
 			while(rs.next())
 			{
 
@@ -65,8 +113,11 @@ public class V_ViewManagementEngine {
 				serviceid= rs.getInt(16);
 				servicename= (String)rs.getString(17);
 				email= (String)rs.getString(18);
-
-
+				
+				/***************************************************************************************/
+				/************ ----| STRUCTURED FORMAT: CustomerInfoView object |----- ******************/
+				/***************************************************************************************/
+				
 				customerinfoview.add(new CustomerInfoView(
 						customerid,
 						contractid,
@@ -91,14 +142,21 @@ public class V_ViewManagementEngine {
 			resultSet.close();
 
 		} catch (SQLException e) {
-			System.out.println("Failed to get Knowledge Hub data");
+			System.out.println("Failed to get Knowledge Hub data. More details:");
+			System.out.println(e.getMessage());
 		}	
-
 		return customerinfoview;
-
 	}
 
-
+/**
+ * ********************************************************************************************************
+ * This Function is used to get all the details of the customer's past incidents which includes following information.
+ * 	Customer Incident status
+ *  Customer Incident description
+ * ********************************************************************************************************
+ * @param customer
+ * @return
+ */
 	public ObservableList<PastIncidentView> pastincidentviewer(int customer){
 		
 		A_DatabaseCommunicationEngine DCE = new A_DatabaseCommunicationEngine();
@@ -115,15 +173,18 @@ public class V_ViewManagementEngine {
 			String status;
 			Integer customerid;
 			String shorttext;
-
-
+			   /***************************************************************************************/
+			   /********-----| GET ALL THE RECORD ROWS DATA INTO A STRUCTURED FORMAT |------- *********/
+			   /***************************************************************************************/
 			while(rs.next())
 			{
-
 				shorttext= (String)rs.getString(1);
 				status=(String)rs.getString(2);
 				customerid= rs.getInt(3);
-
+				
+				/***************************************************************************************/
+				/************ ----| STRUCTURED FORMAT: PastIncidentView object |----- ******************/
+				/***************************************************************************************/
 				pastincidentinfoview.add(new PastIncidentView(
 						status,
 						customerid,
@@ -133,7 +194,8 @@ public class V_ViewManagementEngine {
 			resultSet.close();
 
 		} catch (SQLException e) {
-			System.out.println("Failed to get Knowledge Hub data");
+			System.out.println("Failed to get Knowledge Hub data. More Details");
+			System.out.println(e.getMessage());
 		}	
 
 		return pastincidentinfoview;
